667902ed5ec9c1a0bf76c9a44e4e48ef89594794
katsutoshi <katsutoshi1156@gmail.com>
2024-05-05 20:22:52 +0900

    tfstateをs3で管理

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,18 @@
 # EC2建てるだけ
 terraform {
+  required_version = ">= 1.2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
-  required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-kamakari"
+    key     = "tastylog-dev-tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
 }
 
 provider "aws" {
